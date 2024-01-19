GerritCodeReview/plugins_events-rabbitmq

commit 7b67529cdddbd28bd63ee9dd87299b4a211bdce2

Add implementation of BrokerApi for RabbitMQ
This change add an implementation of the BrokerApi from the
Gerrit events-broker lib. This make it possible to use
RabbitMQ as the message-broker in a multi-site setup.

The BrokerApi implementation can make use of the default
configuration values in $site_path/data/@PLUGIN@.config but
specific configurations needs to be set in
$site_path/data/@PLUGIN@/broker/broker.config

Added a new section called General with the flags:

* publishAllGerritEvents: This will make the plugin publish gerrit
  stream events to a configured exchange. This was the original purpose
  of this plugin and is therefore enabled by default.
* enableBrokerApi: Enable the RabbitMQ Broker API. This is the new
  functionality and is disabled by default.

Change-Id: I7806bee5a10e6a0724427ff2c1dd065c6af1514c

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Manager.java
@@ -20,8 +20,11 @@ import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
+import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Gerrit;
 import com.googlesource.gerrit.plugins.rabbitmq.message.BaseProperties;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiPublisher;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiSubscribers;
 import com.googlesource.gerrit.plugins.rabbitmq.message.GerritEventPublisherFactory;
 import com.googlesource.gerrit.plugins.rabbitmq.message.Publisher;
 import com.googlesource.gerrit.plugins.rabbitmq.message.PublisherPropertiesProvider;
@@ -41,6 +44,7 @@ public class Manager implements LifecycleListener {
   private final EventWorker userEventWorker;
   private final GerritEventPublisherFactory publisherFactory;
   private final List<Publisher> publisherList = new ArrayList<>();
+  private final BrokerApiSubscribers subscribers;
   private final Properties baseProperties;
   private final PublisherPropertiesProvider publisherPropertiesProvider;
 
@@ -50,33 +54,41 @@ public class Manager implements LifecycleListener {
       final DefaultEventWorker defaultEventWorker,
       final EventWorkerFactory eventWorkerFactory,
       final GerritEventPublisherFactory publisherFactory,
+      final BrokerApiPublisher publisher,
+      final BrokerApiSubscribers subscribers,
       final @BaseProperties Properties baseProperties,
       final PublisherPropertiesProvider publisherPropertiesProvider) {
     this.pluginName = pluginName;
     this.defaultEventWorker = defaultEventWorker;
     this.userEventWorker = eventWorkerFactory.create();
     this.publisherFactory = publisherFactory;
+    this.subscribers = subscribers;
     this.baseProperties = baseProperties;
     this.publisherPropertiesProvider = publisherPropertiesProvider;
+    publisherList.add(publisher);
   }
 
   @Override
   public void start() {
-    for (Properties properties : publisherPropertiesProvider.get()) {
-      Publisher publisher = publisherFactory.create(properties);
-      publisher.start();
-      String listenAs = properties.getSection(Gerrit.class).listenAs;
-      if (!listenAs.isEmpty()) {
-        userEventWorker.addPublisher(pluginName, publisher, listenAs);
-      } else {
-        defaultEventWorker.addPublisher(publisher);
+    if (baseProperties.getSection(General.class).publishAllGerritEvents) {
+      logger.atFine().log("publishAllGerritEvents enabled");
+      for (Properties properties : publisherPropertiesProvider.get()) {
+        Publisher publisher = publisherFactory.create(properties);
+        publisher.start();
+        String listenAs = properties.getSection(Gerrit.class).listenAs;
+        if (!listenAs.isEmpty()) {
+          userEventWorker.addPublisher(pluginName, publisher, listenAs);
+        } else {
+          defaultEventWorker.addPublisher(publisher);
+        }
+        publisherList.add(publisher);
       }
-      publisherList.add(publisher);
     }
   }
 
   @Override
   public void stop() {
+    subscribers.stop();
     for (Publisher publisher : publisherList) {
       publisher.stop();
     }

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Module.java
@@ -19,6 +19,7 @@ import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.server.events.EventListener;
 import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.multibindings.Multibinder;
@@ -27,6 +28,7 @@ import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
 import com.googlesource.gerrit.plugins.rabbitmq.config.PropertiesFactory;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.AMQP;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Exchange;
+import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Gerrit;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Message;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Monitor;
@@ -45,6 +47,12 @@ import com.googlesource.gerrit.plugins.rabbitmq.worker.EventWorkerFactory;
 import com.googlesource.gerrit.plugins.rabbitmq.worker.UserEventWorker;
 
 class Module extends AbstractModule {
+  private final RabbitMqBrokerApiModule rabbitMqBrokerApiModule;
+
+  @Inject
+  public Module(RabbitMqBrokerApiModule rabbitMqBrokerApiModule) {
+    this.rabbitMqBrokerApiModule = rabbitMqBrokerApiModule;
+  }
 
   @Override
   protected void configure() {
@@ -56,6 +64,7 @@ class Module extends AbstractModule {
     sectionBinder.addBinding().to(Gerrit.class);
     sectionBinder.addBinding().to(Message.class);
     sectionBinder.addBinding().to(Monitor.class);
+    sectionBinder.addBinding().to(General.class);
 
     install(
         new FactoryModuleBuilder()
@@ -77,5 +86,7 @@ class Module extends AbstractModule {
     bind(Properties.class)
         .annotatedWith(BaseProperties.class)
         .toProvider(BasePropertiesProvider.class);
+
+    install(rabbitMqBrokerApiModule);
   }
 }

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/RabbitMqBrokerApiModule.java
@@ -1,0 +1,40 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.rabbitmq;
+
+import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.lifecycle.LifecycleModule;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiProperties;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiPropertiesProvider;
+
+@Singleton
+public class RabbitMqBrokerApiModule extends LifecycleModule {
+
+  @Inject
+  public RabbitMqBrokerApiModule() {}
+
+  @Override
+  protected void configure() {
+    bind(Properties.class)
+        .annotatedWith(BrokerApiProperties.class)
+        .toProvider(BrokerApiPropertiesProvider.class);
+
+    DynamicItem.bind(binder(), BrokerApi.class).to(RabbitMqBrokerApi.class);
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/BrokerApiPublisher.java
@@ -1,0 +1,69 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.rabbitmq.message;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.events.EventGson;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
+import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
+import com.googlesource.gerrit.plugins.rabbitmq.session.SessionFactoryProvider;
+
+@Singleton
+public class BrokerApiPublisher extends MessagePublisher {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final boolean enabled;
+
+  @Inject
+  public BrokerApiPublisher(
+      SessionFactoryProvider sessionFactoryProvider,
+      @EventGson Gson gson,
+      @BrokerApiProperties Properties properties) {
+    super(properties, sessionFactoryProvider, gson);
+    this.enabled = properties.getSection(General.class).enableBrokerApi;
+  }
+
+  @Override
+  public void start() {
+    if (enabled) {
+      logger.atFine().log("BrokerApiPublisher is starting");
+      super.start();
+    } else {
+      logger.atWarning().log(
+          "The RabbitMqBrokerApi is disabled, set enableBrokerApi to true to enable");
+    }
+  }
+
+  @Override
+  public void stop() {
+    logger.atFine().log("BrokerApiPublisher is getting stopped");
+    super.stop();
+  }
+
+  @Override
+  public ListenableFuture<Boolean> publish(String topic, Event event) {
+    if (enabled) {
+      logger.atFine().log("Message sent to topic %s with data: %s", topic, event);
+      return super.publish(topic, event);
+    }
+    return null;
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/BrokerApiSubscribers.java
@@ -1,0 +1,101 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.rabbitmq.message;
+
+import static com.gerritforge.gerrit.eventbroker.TopicSubscriber.topicSubscriber;
+
+import com.gerritforge.gerrit.eventbroker.TopicSubscriber;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.events.EventGson;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
+import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
+import com.googlesource.gerrit.plugins.rabbitmq.session.Session;
+import com.googlesource.gerrit.plugins.rabbitmq.session.SessionFactoryProvider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+public class BrokerApiSubscribers {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final Session session;
+  private final Properties properties;
+  private final Gson gson;
+  private final boolean enabled;
+  private final Map<TopicSubscriber, String> consumerTags = new HashMap<>();
+
+  @Inject
+  public BrokerApiSubscribers(
+      SessionFactoryProvider sessionFactoryProvider,
+      @EventGson Gson gson,
+      @BrokerApiProperties Properties properties) {
+    this.properties = properties;
+    this.session = sessionFactoryProvider.get().create(properties);
+    this.gson = gson;
+    this.enabled = properties.getSection(General.class).enableBrokerApi;
+  }
+
+  public void stop() {
+    if (session.isOpen()) {
+      logger.atFine().log("Stopping BrokerApiSubscribers...");
+      session.disconnect();
+    } else {
+      logger.atFine().log("BrokerApiSubscribers is already stopped");
+    }
+  }
+
+  public boolean addSubscriber(TopicSubscriber topicSubscriber) {
+    String topic = topicSubscriber.topic();
+    if (enabled) {
+      logger.atFine().log("RabbitMqBrokerApi used to set consumer to topic %s", topic);
+      String consumerTag =
+          session.addSubscriber(
+              topic,
+              messageBody -> {
+                logger.atFiner().log(
+                    "The RabbitMqBrokerApi consumed event from topic %s with data: %s",
+                    topic, messageBody);
+                Event event = gson.fromJson(messageBody, Event.class);
+                if (event.type != null) {
+                  topicSubscriber.consumer().accept(event);
+                } else {
+                  logger.atFine().log("Event does not have a type, ignoring Event");
+                }
+              });
+      if (consumerTag != null) {
+        consumerTags.put(topicSubscriber, consumerTag);
+        return true;
+      }
+    } else {
+      logger.atWarning().log(
+          "The RabbitMqBrokerApi is disabled, set enableBrokerApi to true to enable");
+    }
+    return false;
+  }
+
+  public boolean removeSubscriber(TopicSubscriber topicSubscriber) {
+    String consumerTag = consumerTags.remove(topicSubscriber);
+    if (consumerTag == null) {
+      logger.atWarning().log("Could not find consumerTag when trying to remove subscriber");
+      return false;
+    }
+    return session.removeSubscriber(consumerTag);
+  }
+}

BUILD
@@ -16,5 +16,6 @@ gerrit_plugin(
         "@commons-io//jar",
         "@commons-lang3//jar:neverlink",
         "@gson//jar:neverlink",
+        "@events-broker//jar",
     ],
 )

external_plugin_deps.bzl
@@ -6,3 +6,9 @@ def external_plugin_deps():
         artifact = "com.rabbitmq:amqp-client:5.10.0",
         sha1 = "4de351467a13b8ca4eb7e8023032f9f964a21796",
     )
+
+    maven_jar(
+        name = "events-broker",
+        artifact = "com.gerritforge:events-broker:3.7.2",
+        sha1 = "3b387b3bd134bed3e4bbd743a69411b05c86461a",
+    )

commit 786d1222e343c1aa4410266e1c511c7c93b15c7a

Remove strict events-broker plugin dependency
This fixes a problem where you would get a NoClassFoundError
when loading the plugin if the events-broker plugin had not
been installed.

A previous commit 7b67529 introduced an accidental
requirement on the events-broker plugin even if not using
the rabbitMQBroker API.

Change-Id: I031572b4a36cefc0a1b003d35eb75882844b3424

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/BrokerApiManager.java
@@ -1,0 +1,43 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.rabbitmq;
+
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiPublisher;
+import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiSubscribers;
+
+@Singleton
+public class BrokerApiManager implements LifecycleListener {
+
+  private final BrokerApiPublisher publisher;
+  private final BrokerApiSubscribers subscribers;
+
+  @Inject
+  public BrokerApiManager(BrokerApiPublisher publisher, BrokerApiSubscribers subscribers) {
+    this.publisher = publisher;
+    this.subscribers = subscribers;
+  }
+
+  @Override
+  public void start() {}
+
+  @Override
+  public void stop() {
+    subscribers.stop();
+    publisher.stop();
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Manager.java
@@ -23,8 +23,6 @@ import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
 import com.googlesource.gerrit.plugins.rabbitmq.config.section.Gerrit;
 import com.googlesource.gerrit.plugins.rabbitmq.message.BaseProperties;
-import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiPublisher;
-import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiSubscribers;
 import com.googlesource.gerrit.plugins.rabbitmq.message.GerritEventPublisherFactory;
 import com.googlesource.gerrit.plugins.rabbitmq.message.Publisher;
 import com.googlesource.gerrit.plugins.rabbitmq.message.PublisherPropertiesProvider;
@@ -44,7 +42,6 @@ public class Manager implements LifecycleListener {
   private final EventWorker userEventWorker;
   private final GerritEventPublisherFactory publisherFactory;
   private final List<Publisher> publisherList = new ArrayList<>();
-  private final BrokerApiSubscribers subscribers;
   private final Properties baseProperties;
   private final PublisherPropertiesProvider publisherPropertiesProvider;
 
@@ -54,18 +51,14 @@ public class Manager implements LifecycleListener {
       final DefaultEventWorker defaultEventWorker,
       final EventWorkerFactory eventWorkerFactory,
       final GerritEventPublisherFactory publisherFactory,
-      final BrokerApiPublisher publisher,
-      final BrokerApiSubscribers subscribers,
       final @BaseProperties Properties baseProperties,
       final PublisherPropertiesProvider publisherPropertiesProvider) {
     this.pluginName = pluginName;
     this.defaultEventWorker = defaultEventWorker;
     this.userEventWorker = eventWorkerFactory.create();
     this.publisherFactory = publisherFactory;
-    this.subscribers = subscribers;
     this.baseProperties = baseProperties;
     this.publisherPropertiesProvider = publisherPropertiesProvider;
-    publisherList.add(publisher);
   }
 
   @Override
@@ -88,7 +81,6 @@ public class Manager implements LifecycleListener {
 
   @Override
   public void stop() {
-    subscribers.stop();
     for (Publisher publisher : publisherList) {
       publisher.stop();
     }

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/Module.java
@@ -14,6 +14,9 @@
 
 package com.googlesource.gerrit.plugins.rabbitmq;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.annotations.PluginData;
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.server.events.EventListener;
@@ -47,13 +50,26 @@ import com.googlesource.gerrit.plugins.rabbitmq.worker.DefaultEventWorker;
 import com.googlesource.gerrit.plugins.rabbitmq.worker.EventWorker;
 import com.googlesource.gerrit.plugins.rabbitmq.worker.EventWorkerFactory;
 import com.googlesource.gerrit.plugins.rabbitmq.worker.UserEventWorker;
+import java.io.File;
+import java.io.IOException;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
 
 class Module extends AbstractModule {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
   private final RabbitMqBrokerApiModule rabbitMqBrokerApiModule;
+  private final boolean brokerApiEnabled;
 
   @Inject
-  public Module(RabbitMqBrokerApiModule rabbitMqBrokerApiModule) {
+  public Module(
+      RabbitMqBrokerApiModule rabbitMqBrokerApiModule,
+      @PluginName final String pluginName,
+      @PluginData final File pluginData) {
     this.rabbitMqBrokerApiModule = rabbitMqBrokerApiModule;
+    this.brokerApiEnabled =
+        getBaseConfig(pluginName, pluginData).getBoolean("General", "enableBrokerApi", false);
   }
 
   @Override
@@ -96,6 +112,23 @@ class Module extends AbstractModule {
         .annotatedWith(BaseProperties.class)
         .toProvider(BasePropertiesProvider.class);
 
-    install(rabbitMqBrokerApiModule);
+    if (brokerApiEnabled) {
+      install(rabbitMqBrokerApiModule);
+    } else {
+      logger.atInfo().log(
+          "The RabbitMqBrokerApi is disabled, set enableBrokerApi to true if you want to enable it");
+    }
+  }
+
+  private FileBasedConfig getBaseConfig(String pluginName, File pluginData) {
+    File baseConfigFile =
+        pluginData.toPath().resolve(pluginName + BasePropertiesProvider.FILE_EXT).toFile();
+    FileBasedConfig config = new FileBasedConfig(baseConfigFile, FS.DETECTED);
+    try {
+      config.load();
+    } catch (IOException | ConfigInvalidException e) {
+      logger.atInfo().withCause(e).log("Unable to load %s", baseConfigFile.getAbsolutePath());
+    }
+    return config;
   }
 }

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/RabbitMqBrokerApiModule.java
@@ -15,7 +15,9 @@
 package com.googlesource.gerrit.plugins.rabbitmq;
 
 import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -31,6 +33,7 @@ public class RabbitMqBrokerApiModule extends LifecycleModule {
 
   @Override
   protected void configure() {
+    DynamicSet.bind(binder(), LifecycleListener.class).to(BrokerApiManager.class);
     bind(Properties.class)
         .annotatedWith(BrokerApiProperties.class)
         .toProvider(BrokerApiPropertiesProvider.class);

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/BasePropertiesProvider.java
@@ -27,7 +27,6 @@ import java.nio.file.Path;
 
 @Singleton
 public class BasePropertiesProvider implements Provider<Properties> {
-
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   public static final String FILE_EXT = ".config";

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/BrokerApiPublisher.java
@@ -22,15 +22,11 @@ import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
-import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
 import com.googlesource.gerrit.plugins.rabbitmq.session.type.AMQPPublisherSession;
 
 @Singleton
 public class BrokerApiPublisher extends MessagePublisher {
-
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-
-  private final boolean enabled;
 
   @Inject
   public BrokerApiPublisher(
@@ -38,18 +34,12 @@ public class BrokerApiPublisher extends MessagePublisher {
       @EventGson Gson gson,
       @BrokerApiProperties Properties properties) {
     super(properties, sessionFactory, gson);
-    this.enabled = properties.getSection(General.class).enableBrokerApi;
   }
 
   @Override
   public void start() {
-    if (enabled) {
-      logger.atFine().log("BrokerApiPublisher is starting");
-      super.start();
-    } else {
-      logger.atWarning().log(
-          "The RabbitMqBrokerApi is disabled, set enableBrokerApi to true to enable");
-    }
+    logger.atFine().log("BrokerApiPublisher is starting");
+    super.start();
   }
 
   @Override
@@ -60,10 +50,7 @@ public class BrokerApiPublisher extends MessagePublisher {
 
   @Override
   public ListenableFuture<Boolean> publish(String topic, Event event) {
-    if (enabled) {
-      logger.atFine().log("Message sent to topic %s with data: %s", topic, event);
-      return super.publish(topic, event);
-    }
-    return null;
+    logger.atFine().log("Message sent to topic %s with data: %s", topic, event);
+    return super.publish(topic, event);
   }
 }

src/main/java/com/googlesource/gerrit/plugins/rabbitmq/message/BrokerApiSubscribers.java
@@ -24,7 +24,6 @@ import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.rabbitmq.config.Properties;
-import com.googlesource.gerrit.plugins.rabbitmq.config.section.General;
 import com.googlesource.gerrit.plugins.rabbitmq.session.SubscriberSession;
 import com.googlesource.gerrit.plugins.rabbitmq.session.type.AMQPSubscriberSession;
 import java.util.HashMap;
@@ -32,13 +31,11 @@ import java.util.Map;
 
 @Singleton
 public class BrokerApiSubscribers {
-
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private final SubscriberSession session;
   private final Properties properties;
   private final Gson gson;
-  private final boolean enabled;
   private final Map<TopicSubscriber, String> consumerTags = new HashMap<>();
 
   @Inject
@@ -49,7 +46,6 @@ public class BrokerApiSubscribers {
     this.properties = properties;
     this.session = sessionFactory.create(properties);
     this.gson = gson;
-    this.enabled = properties.getSection(General.class).enableBrokerApi;
   }
 
   public void stop() {
@@ -63,29 +59,24 @@ public class BrokerApiSubscribers {
 
   public boolean addSubscriber(TopicSubscriber topicSubscriber) {
     String topic = topicSubscriber.topic();
-    if (enabled) {
-      logger.atFine().log("RabbitMqBrokerApi used to set consumer to topic %s", topic);
-      String consumerTag =
-          session.addSubscriber(
-              topic,
-              messageBody -> {
-                logger.atFiner().log(
-                    "The RabbitMqBrokerApi consumed event from topic %s with data: %s",
-                    topic, messageBody);
-                Event event = gson.fromJson(messageBody, Event.class);
-                if (event.type != null) {
-                  topicSubscriber.consumer().accept(event);
-                } else {
-                  logger.atFine().log("Event does not have a type, ignoring Event");
-                }
-              });
-      if (consumerTag != null) {
-        consumerTags.put(topicSubscriber, consumerTag);
-        return true;
-      }
-    } else {
-      logger.atWarning().log(
-          "The RabbitMqBrokerApi is disabled, set enableBrokerApi to true to enable");
+    logger.atFine().log("RabbitMqBrokerApi used to set consumer to topic %s", topic);
+    String consumerTag =
+        session.addSubscriber(
+            topic,
+            messageBody -> {
+              logger.atFiner().log(
+                  "The RabbitMqBrokerApi consumed event from topic %s with data: %s",
+                  topic, messageBody);
+              Event event = gson.fromJson(messageBody, Event.class);
+              if (event.type != null) {
+                topicSubscriber.consumer().accept(event);
+              } else {
+                logger.atFine().log("Event does not have a type, ignoring Event");
+              }
+            });
+    if (consumerTag != null) {
+      consumerTags.put(topicSubscriber, consumerTag);
+      return true;
     }
     return false;
   }