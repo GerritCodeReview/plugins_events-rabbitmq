d73c3ca6365a9c1beef1a9e2617d713321018e52
Rikard Almgren <rikardal@axis.com>
2025-03-12 13:56:13 +0100

    Add consumers from previous broker API implementations
    When using event brokers, the event-broker lib provides a default
    brokerapi implementation to facilitate startup.
    
    When events-rabbitmq is loaded, we should adopt all consumers registered
    to the previous broker api.
    
    This allows us to load events-rabbitmq after plugins that set up
    consumers and help avoid potentially losing events.
    
    Added some logging to help understanding load orders.
    
    Draws heavy inspiration from events-kafka's implementation.
    
    Change-Id: Iaffdbc662bee17de1714cbe19048c2d740805984

--- src/main/java/com/googlesource/gerrit/plugins/rabbitmq/BrokerApiManager.java
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/BrokerApiManager.java
@@ -14,29 +14,51 @@
 
 package com.googlesource.gerrit.plugins.rabbitmq;
 
+import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.TopicSubscriber;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiPublisher;
 import com.googlesource.gerrit.plugins.rabbitmq.message.BrokerApiSubscribers;
+import com.google.common.flogger.FluentLogger;
+import java.util.Set;
 
 @Singleton
 public class BrokerApiManager implements LifecycleListener {
 
+  private final FluentLogger logger = FluentLogger.forEnclosingClass();
   private final BrokerApiPublisher publisher;
   private final BrokerApiSubscribers subscribers;
+  private final Set<TopicSubscriber> consumers;
+  private final BrokerApi brokerApi;
 
   @Inject
-  public BrokerApiManager(BrokerApiPublisher publisher, BrokerApiSubscribers subscribers) {
+  public BrokerApiManager(
+      BrokerApiPublisher publisher,
+      BrokerApiSubscribers subscribers,
+      Set<TopicSubscriber> consumers,
+      BrokerApi brokerApi) {
+    logger.atFine().log("BrokerAPIManager Initialized");
     this.publisher = publisher;
     this.subscribers = subscribers;
+    this.consumers = consumers;
+    this.brokerApi = brokerApi;
   }
 
   @Override
-  public void start() {}
+  public void start() {
+    logger.atInfo().log("BrokerApiManager started and loading existing subscribers");
+    consumers.forEach(
+        topicSubscriber ->
+            brokerApi.receiveAsync(topicSubscriber.topic(), topicSubscriber.consumer()));
+    logger.atInfo().log(
+        "RabbitMQ broker started with %d topic subscribers", brokerApi.topicSubscribers().size());
+  }
 
   @Override
   public void stop() {
+    logger.atInfo().log("BrokerApiManager Stopping");
     subscribers.stop();
     publisher.stop();
   }
