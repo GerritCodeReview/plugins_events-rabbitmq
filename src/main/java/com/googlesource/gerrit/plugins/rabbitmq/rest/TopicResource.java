6ebb54e113ed134139a2b30821300da9aa62b6a0
David Åkerman <davidak@axis.com>
2025-09-18 19:46:54 +0200

    Add REST API endpoints for modifying stream offsets for topics
    Change-Id: I9cce853fdd233c1324e846c84c1fd1c99971e54e

---
+++ src/main/java/com/googlesource/gerrit/plugins/rabbitmq/rest/TopicResource.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.rabbitmq.rest;
+
+import com.google.gerrit.extensions.restapi.RestResource;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.config.ConfigResource;
+import com.google.inject.TypeLiteral;
+
+/** Resource representing a specific topic in the RabbitMQ plugin. */
+public class TopicResource implements RestResource {
+  public static final TypeLiteral<RestView<TopicResource>> TOPIC_KIND = 
+      new TypeLiteral<RestView<TopicResource>>() {};
+
+  private final String topicName;
+
+  public TopicResource(String topicName) {
+    this.topicName = topicName;
+  }
+
+  public String getTopicName() {
+    return topicName;
+  }
+}
